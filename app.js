7e680ec43e3cdc4e4e5773e57975dd2a20dee743
IrynaZharnosek <50871778+IrynaZharnosek@users.noreply.github.com>
2019-05-30 16:54:00 +0300

    Update app.js

--- app.js
+++ app.js
@@ -1,1 +1,13 @@
+const format = require('node.date-time');
 
+var fs = require('fs');
+var message = 'Фиксирую данные';
+
+//функция получения текущей даты и времени в нужном формате подключенного модуля node.date-time
+function logTime(){
+	return new Date().format('Y-M-d H:M:S') + '-';
+}
+//выполнение записи в файл с логами каждые 5 сек
+setInterval(function(){
+	fs.appendFile('file.log', logTime()+message+'\n');
+}, 5000);
